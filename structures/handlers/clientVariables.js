21c7c5d67cbcb66421a4b16df612c6b312dc8e1e
Zedro2742 <61875966+Zedro2742@users.noreply.github.com>
2022-02-01 12:29:58 +0530

    Update clientVariables.js

--- structures/handlers/clientVariables.js
+++ structures/handlers/clientVariables.js
@@ -3,13 +3,13 @@ const config = require("../botconfig/config.json");
 
 module.exports = async (client) => {
 
-    client.commands = new Collection();
+   	client.commands = new Collection();
 	client.slashCommands = new Collection();
 	client.events = new Collection();
 	client.aliases = new Collection();
 	client.cooldowns = new Collection();
-    client.allEmojis = require("../botconfig/emojis.json");
-    client.owners = config.ownerID;
+   	client.allEmojis = require("../botconfig/emojis.json");
+   	client.owners = config.ownerID;
 
 }
 
